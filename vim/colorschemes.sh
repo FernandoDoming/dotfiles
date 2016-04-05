53461584cd6f8216fff0e25c27c4d76485d4c25d
Fernando Domínguez <fernan__93@hotmail.com>
2016-04-05 07:42:07 +0000

    Add vim colors

---
+++ vim/colorschemes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/zenburn.vim https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
+curl -o ~/.vim/colors/luna.vim https://raw.githubusercontent.com/notpratheek/vim-luna/master/colors/luna.vim
+curl -o ~/.vim/colors/luna-term.vim https://raw.githubusercontent.com/notpratheek/vim-luna/master/colors/luna-term.vim
