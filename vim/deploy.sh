5723f0c5e6aa6c8872d023bd59be9b8ac45afa40
Fernando <fernando@fernandodominguez.me>
2016-04-06 11:31:16 -0400

    vim autoinstall

--- vim/deploy.sh
+++ vim/deploy.sh
@@ -4,3 +4,6 @@ mkdir -p ~/.vim/colors
 curl -o ~/.vim/colors/zenburn.vim https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
 curl -o ~/.vim/colors/luna.vim https://raw.githubusercontent.com/notpratheek/vim-luna/master/colors/luna.vim
 curl -o ~/.vim/colors/luna-term.vim https://raw.githubusercontent.com/notpratheek/vim-luna/master/colors/luna-term.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c "VundleInstall"
