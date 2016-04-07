4e40cf0be587fcaabf7e4f6c2b1f7fe5d7266415
Fernando Dominguez <fernan__93@hotmail.com>
2016-04-07 07:52:00 -0400

    Add autoclose plugin

--- vim/deploy.sh
+++ vim/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 mkdir -p ~/.vim/colors
+mkdir -p ~/.vim/plugin
+
 curl -o ~/.vim/colors/zenburn.vim https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
 curl -o ~/.vim/colors/luna.vim https://raw.githubusercontent.com/notpratheek/vim-luna/master/colors/luna.vim
 curl -o ~/.vim/colors/luna-term.vim https://raw.githubusercontent.com/notpratheek/vim-luna/master/colors/luna-term.vim
+curl http://www.vim.org/scripts/download_script.php?src_id=9223 -o ~/.vim/plugin/autoclose.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 vim -c "VundleInstall"
